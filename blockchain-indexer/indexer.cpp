bb187f0c6634a85dc906a2e69d943f24222bc40f
grillow <spmr757@gmail.com>
2022-06-15 12:30:11 +0300

    temp

--- blockchain-indexer/indexer.cpp
+++ blockchain-indexer/indexer.cpp
@@ -1127,8 +1127,8 @@ class Indexer : public td::actor::Actor {
 
   void display_progress_chain() {
     display_progress();
-    delay_action([SelfId = actor_id(this)](){
-      td::actor::send_closure(SelfId, &Indexer::display_progress_chain);
+    delay_action([this](){
+      display_progress();
     }, td::Timestamp::in(1));
   }
 
