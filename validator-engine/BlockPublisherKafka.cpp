641afd352979a66f54de47563883c670901c17fe
grillow <spmr757@gmail.com>
2022-07-19 17:27:01 +0300

    producer.flush();

--- validator-engine/BlockPublisherKafka.cpp
+++ validator-engine/BlockPublisherKafka.cpp
@@ -12,12 +12,14 @@ void BlockPublisherKafka::publishBlockData(const std::string& json) {
   std::lock_guard<std::mutex> guard(net_mtx);
   LOG(INFO) << "Sending " << json.size() << " bytes to Kafka";
   producer.produce(cppkafka::MessageBuilder("block-data").partition(0).payload(json));
+  producer.flush();
 }
 
 void BlockPublisherKafka::publishBlockState(const std::string& json) {
   std::lock_guard<std::mutex> guard(net_mtx);
   LOG(INFO) << "Sending " << json.size() << " bytes to Kafka";
   producer.produce(cppkafka::MessageBuilder("block-state").partition(0).payload(json));
+  producer.flush();
 }
 
 }
