31cc93b53e8138d03424b0cd5c195f65ffd0a0cb
grillow <spmr757@gmail.com>
2022-07-25 18:13:45 +0300

    kafka max size 2 GB -> 1 GB

--- validator-engine/BlockPublisherKafka.cpp
+++ validator-engine/BlockPublisherKafka.cpp
@@ -6,7 +6,7 @@ namespace ton::validator {
 BlockPublisherKafka::BlockPublisherKafka(const std::string& endpoint) : producer(
     cppkafka::Configuration{
         { "metadata.broker.list", endpoint },
-        { "message.max.bytes", "2147483648"}    // 2 GB
+        { "message.max.bytes", "1073741824"}    // 1 GB
     }
 ) {}
 
