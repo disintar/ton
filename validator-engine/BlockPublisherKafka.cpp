8fc1c997b20c54f49eb38d0214c6d761538d447a
Andrey Tvorozhkov <andrey@head-labs.com>
2022-12-14 08:19:05 +0300

    Add env for topics

--- validator-engine/BlockPublisherKafka.cpp
+++ validator-engine/BlockPublisherKafka.cpp
@@ -3,20 +3,19 @@
 
 namespace ton::validator {
 
-BlockPublisherKafka::BlockPublisherKafka(const std::string& endpoint) : producer(
-    cppkafka::Configuration{
-        { "metadata.broker.list", endpoint },
-        { "message.max.bytes", "1000000000" },  // max
-        { "acks", "1" },
-        {"debug","msg,broker,topic"}
-    }
-) {}
+BlockPublisherKafka::BlockPublisherKafka(const std::string& endpoint)
+    : producer(cppkafka::Configuration{{"metadata.broker.list", endpoint},
+                                       {"message.max.bytes", "1000000000"},  // max
+                                       {"acks", "1"},
+                                       {"debug", "msg,broker,topic"}}) {
+}
 
 void BlockPublisherKafka::publishBlockApplied(std::string json) {
   std::lock_guard<std::mutex> guard(net_mtx);
   LOG(INFO) << "[block-applied] Sending " << json.size() << " bytes to Kafka";
   try {
-    producer.produce(cppkafka::MessageBuilder("block-applied-mainnet").partition(0).payload(json));
+    const char* value = getenv("KAFKA_APPLY_TOPIC");
+    producer.produce(cppkafka::MessageBuilder(value ? value : "block-applied-mainnet").partition(0).payload(json));
     producer.flush(std::chrono::milliseconds(100000));
   } catch (std::exception& e) {
     const auto id = to_string(json::parse(json)["id"]);
@@ -29,7 +28,8 @@ void BlockPublisherKafka::publishBlockData(std::string json) {
   std::lock_guard<std::mutex> guard(net_mtx);
   LOG(INFO) << "[block-data] Sending " << json.size() << " bytes to Kafka";
   try {
-    producer.produce(cppkafka::MessageBuilder("block-data-mainnet").partition(0).payload(json));
+    const char* value = getenv("KAFKA_BLOCK_TOPIC");
+    producer.produce(cppkafka::MessageBuilder(value ? value : "block-data-mainnet").partition(0).payload(json));
     producer.flush(std::chrono::milliseconds(100000));
   } catch (std::exception& e) {
     const auto id = to_string(json::parse(json)["id"]);
@@ -42,7 +42,8 @@ void BlockPublisherKafka::publishBlockState(std::string json) {
   std::lock_guard<std::mutex> guard(net_mtx);
   LOG(INFO) << "[block-state] Sending " << json.size() << " bytes to Kafka";
   try {
-    producer.produce(cppkafka::MessageBuilder("block-state-mainnet").partition(0).payload(json));
+    const char* value = getenv("KAFKA_STATE_TOPIC");
+    producer.produce(cppkafka::MessageBuilder(value ? value : "block-state-mainnet").partition(0).payload(json));
     producer.flush(std::chrono::milliseconds(100000));
   } catch (std::exception& e) {
     const auto id = to_string(json::parse(json)["id"]);
@@ -52,19 +53,17 @@ void BlockPublisherKafka::publishBlockState(std::string json) {
 }
 
 void BlockPublisherKafka::publishBlockError(const std::string& id, const std::string& error) {
-  const json json = {
-      {"id", id},
-      {"error", error}
-  };
+  const json json = {{"id", id}, {"error", error}};
   const std::string dump = json.dump();
 
   try {
+    const char* value = getenv("KAFKA_ERROR_TOPIC");
     LOG(WARNING) << "[block-error] Sending " << json.size() << " bytes to Kafka";
-    producer.produce(cppkafka::MessageBuilder("block-error-mainnet").partition(0).payload(dump));
+    producer.produce(cppkafka::MessageBuilder(value ? value : "block-error-mainnet").partition(0).payload(dump));
     producer.flush(std::chrono::milliseconds(10000));
   } catch (std::exception& e) {
     LOG(ERROR) << "Error while sending block (" << id << ") error (" << error << ") to kafka: " << e.what();
   }
 }
 
-}
+}  // namespace ton::validator
