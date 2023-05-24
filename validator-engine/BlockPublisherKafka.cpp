e3f914e47b1f0f2f385550b468e729a683151324
Andrey Tvorozhkov <andrey@head-labs.com>
2023-05-24 12:44:34 +0300

    Remove queue setting

--- validator-engine/BlockPublisherKafka.cpp
+++ validator-engine/BlockPublisherKafka.cpp
@@ -8,7 +8,6 @@ BlockPublisherKafka::BlockPublisherKafka(const std::string& endpoint)
                                        {"message.max.bytes", "1000000000"},  // max
                                        {"acks", "1"},
                                        {"debug", "msg,broker,topic"},
-                                       {"queue.buffering.max.ms", "2000"},
       }) {
 }
 
