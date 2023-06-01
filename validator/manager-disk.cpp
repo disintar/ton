cd5fdfe5b3a1f243706f2792b57eba387448c7bc
Andrey Tvorozhkov <andrey@head-labs.com>
2023-06-01 17:53:42 +0300

    Fix

--- validator/manager-disk.cpp
+++ validator/manager-disk.cpp
@@ -583,7 +583,6 @@ void ValidatorManagerImpl::get_shard_state_from_db(ConstBlockHandle handle, td::
 
 void ValidatorManagerImpl::get_shard_state_root_cell_from_db(ConstBlockHandle handle,
                                                              td::Promise<td::Ref<vm::DataCell>> promise) {
-  LOG(WARNING) << "Special work for: " << handle->id().to_str();
   td::actor::send_closure(db_, &Db::get_block_state_root_cell, handle, std::move(promise));
 }
 
