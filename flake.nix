00af9122e68dc7c6e59cf7a91566d017bdc2b64e
tonthemoon <tonthemoon@mailbox.org>
2023-06-21 13:59:01 +0000

    nix: Static secp256k1

--- flake.nix
+++ flake.nix
@@ -38,7 +38,7 @@
                 (openssl_1_1.override { static = true; }).dev
                 (zlib.override { shared = false; }).dev
             ]
-            ++ optionals (!stdenv.isDarwin) [ pkgsStatic.libmicrohttpd.dev pkgsStatic.libsodium.dev secp256k1 ]
+            ++ optionals (!stdenv.isDarwin) [ pkgsStatic.libmicrohttpd.dev pkgsStatic.libsodium.dev pkgsStatic.secp256k1 ]
             ++ optionals stdenv.isDarwin [ (libiconv.override { enableStatic = true; enableShared = false; }) ]
             ++ optionals stdenv.isDarwin (forEach [ libmicrohttpd.dev libsodium.dev secp256k1 gmp.dev nettle.dev (gnutls.override { withP11-kit = false; }).dev libtasn1.dev libidn2.dev libunistring.dev gettext ] (x: x.overrideAttrs(oldAttrs: rec { configureFlags = (oldAttrs.configureFlags or []) ++ [ "--enable-static" "--disable-shared" ]; dontDisableStatic = true; })))
             ++ optionals staticGlibc [ glibc.static ];
