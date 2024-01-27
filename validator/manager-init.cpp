e2365cadba89bfd0246e4359d31191e8c435c5de
Andrey Tvorozhkov <andrey@head-labs.com>
2024-01-28 00:16:02 +0300

    Fix

--- validator/manager-init.cpp
+++ validator/manager-init.cpp
@@ -367,7 +367,7 @@ void ValidatorManagerMasterchainStarter::got_init_block_state(td::Ref<Masterchai
   state_ = std::move(state);
   CHECK(state_->get_block_id() == opts_->init_block_id() || state_->ancestor_is_valid(opts_->init_block_id()) ||
         state_->get_block_id().seqno() < opts_->get_last_fork_masterchain_seqno());
-  finish();
+  //finish();
 
   auto P = td::PromiseCreator::lambda(
       [SelfId = actor_id(this), block_id = opts_->init_block_id()](td::Result<BlockIdExt> R) {
