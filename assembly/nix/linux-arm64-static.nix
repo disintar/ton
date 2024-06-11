40ecd4c0c1ad3ad0d080ea46d2266a8cd3710454
Andrey Tvorozhkov <andrey@head-labs.com>
2024-06-11 16:13:08 +0300

    Fix nix

--- assembly/nix/linux-arm64-static.nix
+++ assembly/nix/linux-arm64-static.nix
@@ -6,6 +6,15 @@
 , system ? builtins.currentSystem
 , src ? ./.
 }:
+
+let
+  boostStatic = pkgs.boost.overrideAttrs (oldAttrs: {
+    buildInputs = (oldAttrs.buildInputs or []) ++ [ pkgsStatic.stdenv ];
+    doCheck = false;
+    configureFlags = [ "--with-libraries=all" "--with-icu" "--with-serialization" "--with-date_time" "--with-thread" "--with-regex" "--with-filesystem" "--with-program_options" "--with-system" "--with-chrono" "--with-random" "--with-test" ];
+  });
+in
+
 let
   microhttpdmy = (import ./microhttpd.nix) { inherit pkgs; };
 in
@@ -29,7 +38,7 @@ stdenv.mkDerivation {
         pkgsStatic.libsodium.dev
         pkgsStatic.secp256k1
         glibc.static
-        boost
+        boostStatic
       ];
 
   makeStatic = true;
@@ -45,6 +54,10 @@ stdenv.mkDerivation {
     "-DMHD_INCLUDE_DIR=${microhttpdmy}/usr/local/include"
     "-DMHD_LIBRARY=${microhttpdmy}/usr/local/lib/libmicrohttpd.a"
     "-DCMAKE_CTEST_ARGUMENTS=--timeout;1800"
+    "-DBOOST_ROOT=${boostStatic}"
+    "-DBOOST_INCLUDEDIR=${boostStatic.dev}/include"
+    "-DBOOST_LIBRARYDIR=${boostStatic.lib}/lib"
+    "-DBoost_USE_STATIC_LIBS=ON"
   ];
 
   LDFLAGS = [
