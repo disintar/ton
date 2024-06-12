850cb51acad992684073e1dd11bd120beccdaa80
Andrey Tvorozhkov <andrey@head-labs.com>
2024-06-12 22:04:57 +0300

    Fix bost

--- assembly/nix/static-libs.nix
+++ assembly/nix/static-libs.nix
@@ -1,19 +1,20 @@
 { pkgs }:
 
 let
-    staticBoost = pkgs.boost.overrideAttrs (oldAttrs: {
-      buildInputs = (oldAttrs.buildInputs or []) ++ [ pkgs.stdenv ];
-      doCheck = false;
-      configurePhase = '' ''; # No configure phase
-      buildPhase = ''
-        ./bootstrap.sh --prefix=$out --with-toolset=clang
-        ./b2 install --prefix=$out --build-dir=build --layout=system link=static threading=multi runtime-link=static
-      '';
-      postInstall = ''
-        rm -rf $out/include/boost
-        mv $oldAttrs.out/include/boost $out/include/
-      '';
-    });
+  staticBoost = pkgs.boost.overrideAttrs (oldAttrs: {
+    buildInputs = (oldAttrs.buildInputs or []) ++ [ pkgs.stdenv ];
+    doCheck = false;
+    configurePhase = '' ''; # No configure phase
+    buildPhase = ''
+      ./bootstrap.sh --prefix=$out --with-toolset=clang
+      ./b2 install --prefix=$out --build-dir=build --layout=system link=static threading=multi runtime-link=static
+    '';
+    postInstall = ''
+      mv $out/include/boost $out/include/boost-moved
+      mv $out/include/boost-moved/* $out/include/
+      rmdir $out/include/boost-moved
+    '';
+  });
 
 
   staticLibrdkafka = pkgs.rdkafka.overrideAttrs (oldAttrs: {
