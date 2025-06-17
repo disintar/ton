e90f1bff35cb2ff97f3472bff8c0cd18c8e10c7a
Andrey Tvorozhkov <andrey@head-labs.com>
2025-06-17 18:41:52 +0300

    fix

--- assembly/nix/linux-x86-64-tonlib.nix
+++ assembly/nix/linux-x86-64-tonlib.nix
@@ -1,8 +1,12 @@
-{ system ? builtins.currentSystem }:
+{
+  pkgs ? import <nixpkgs> { inherit system; }
+, lib ? pkgs.lib
+, stdenv ? pkgs.stdenv
+, system ? builtins.currentSystem
+, src ? ./.
+}:
 
 let
-  pkgs = import <nixpkgs> { inherit system; };
-
   nixos1909 = import (builtins.fetchTarball {
     url = "https://channels.nixos.org/nixos-19.09/nixexprs.tar.xz";
     sha256 = "1vp1h2gkkrckp8dzkqnpcc6xx5lph5d2z46sg2cwzccpr8ay58zy";
@@ -10,31 +14,35 @@ let
 
   glibc227 = nixos1909.glibc // { pname = "glibc"; };
 
-  cc = pkgs.wrapCCWith {
-    cc = nixos1909.buildPackages.gcc-unwrapped;
-    libc = glibc227;
-    bintools = pkgs.binutils.override { libc = glibc227; };
-  };
-
-  stdenv227 = pkgs.overrideCC pkgs.stdenv cc;
+  clangStdenv = let
+    cc = pkgs.wrapCCWith {
+      cc = pkgs.clang_16;
+      libc = glibc227;
+      bintools = pkgs.binutils.override { libc = glibc227; };
+    };
+  in pkgs.overrideCC pkgs.stdenv cc;
 
   staticLibs = import ./static-libs.nix { inherit pkgs; };
 
-in stdenv227.mkDerivation {
+in
+clangStdenv.mkDerivation {
   pname = "ton";
   version = "dev-lib";
-  src = ./.; # если используешь в git-репозитории
 
-  nativeBuildInputs = with pkgs; [ cmake ninja git pkg-config ];
+  inherit src;
+
+  nativeBuildInputs = with pkgs; [
+    cmake ninja git pkg-config
+  ];
 
   buildInputs = with pkgs; [
     pkgsStatic.openssl
     pkgsStatic.zlib
     pkgsStatic.libmicrohttpd.dev
     pkgsStatic.secp256k1
-    pkgsStatic.lz4
     staticLibs.staticBoost
     staticLibs.staticLibrdkafka
+    pkgsStatic.lz4
     (pkgsStatic.libsodium.overrideAttrs (oldAttrs: {
       configureFlags = oldAttrs.configureFlags ++ [ "--disable-pie" ];
       hardeningDisable = oldAttrs.hardeningDisable ++ [ "pie" ];
@@ -46,20 +54,19 @@ in stdenv227.mkDerivation {
   cmakeFlags = [
     "-DTON_USE_ABSEIL=ON"
     "-DNIX=ON"
-    "-DCMAKE_C_COMPILER=${cc}/bin/cc"
-    "-DCMAKE_CXX_COMPILER=${cc}/bin/c++"
-    "-DCMAKE_CXX_STANDARD=20"
-    "-DCMAKE_CXX_FLAGS=-Wno-deprecated-declarations -Wno-unused-but-set-variable -w"
+    "-DCMAKE_CXX_FLAGS=-w"
     "-DCMAKE_C_FLAGS=-w"
+    "-DCMAKE_CXX_STANDARD=20"
+    "-DCMAKE_CXX_FLAGS=-Wno-deprecated-declarations -Wno-unused-but-set-variable"
   ];
 
   LDFLAGS = [
-    "-static-libgcc"
-    "-static-libstdc++"
-    "-fPIC"
+    "-static-libgcc" "-static-libstdc++" "-fPIC"
   ];
 
-  ninjaFlags = [ "tonlibjson" "emulator" ];
+  ninjaFlags = [
+    "tonlibjson" "emulator"
+  ];
 
   preConfigure = ''
     echo ">>> linux-x86-64-tonlib.nix Checking compiler:"
