a1d0d9d6a3ca16527c8de5783b9417552955dd34
Andrey Tvorozhkov <andrey@head-labs.com>
2025-06-30 14:59:51 +0300

    Build openssl

--- assembly/native/build-ubuntu-static.sh
+++ assembly/native/build-ubuntu-static.sh
@@ -18,12 +18,31 @@ fi
 export CC=$(which clang-16)
 export CXX=$(which clang++-16)
 
+mkdir -p ../3pp
+
+
+if [ ! -d "../3pp/openssl_3" ]; then
+  git clone https://github.com/openssl/openssl ../3pp/openssl_3
+  cd ../3pp/openssl_3
+  opensslPath=`pwd`
+  git checkout openssl-3.1.4
+  ./config  no-shared
+  make build_libs -j$(nproc)
+  test $? -eq 0 || { echo "Can't compile openssl_3"; exit 1; }
+  cd $buildir
+else
+  opensslPath=$(pwd)/../3pp/openssl_3
+  echo "Using compiled openssl_3"
+fi
 
 cmake -GNinja .. \
       -DPORTABLE=1 \
       -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_C_FLAGS="-w -static-libgcc -latomic -I${libmicrohttpdPath}/src/include" \
       -DBUILD_SHARED_LIBS=OFF \
+      -DOPENSSL_FOUND=1 \Add commentMore actions \
+      -DOPENSSL_INCLUDE_DIR=$opensslPath/include \
+      -DOPENSSL_CRYPTO_LIBRARY=$opensslPath/libcrypto.a \
       -DCMAKE_CXX_FLAGS="-w -I${libmicrohttpdPath}/src/include -Bstatic /usr/lib/gcc/x86_64-linux-gnu/11/libatomic.a -static-libgcc -static-libstdc++ -latomic" \
       -DCMAKE_EXE_LINKER_FLAGS="-static -latomic" \
       -DTON_USE_PYTHON=1
