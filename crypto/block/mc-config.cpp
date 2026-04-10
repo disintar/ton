b9c0105b9270879e1d62aff486f0c08869e25e73
Andrey Tvorozhkov <andrey@head-labs.com>
2026-04-10 09:10:58 +0300

    Fix

--- crypto/block/mc-config.cpp
+++ crypto/block/mc-config.cpp
@@ -408,8 +408,8 @@ td::optional<ton::NewConsensusConfig> Config::get_new_consensus_config(ton::Work
 
         .noncritical_params =
             {
-                .target_rate{v1.target_rate_ms},
-                .first_block_timeout{v1.first_block_timeout_ms},
+                .target_rate = std::chrono::milliseconds{v1.target_rate_ms},
+                .first_block_timeout = std::chrono::milliseconds{v1.first_block_timeout_ms},
                 .max_leader_window_desync = v1.max_leader_window_desync,
             },
     };
